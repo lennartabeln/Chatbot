afe57893d15b1012990280ded04a8394319ea24a
Lennart Abeln <lennart.abeln@web.de>
2024-03-14 14:04:09 +0100

    Added levenshteinDistance

--- bot/bot.js
+++ bot/bot.js
@@ -8,7 +8,8 @@ class EchoBot extends ActivityHandler {
         super();
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
-            const replyText = `Echo: ${ context.activity.text }`;
+            const distance = levenshteinDistance(context.activity.text, 'Hallo')
+            const replyText = `${ distance }`;
             await context.sendActivity(MessageFactory.text(replyText, replyText));
             // By calling next() you ensure that the next BotHandler is run.
             await next();
@@ -29,3 +30,37 @@ class EchoBot extends ActivityHandler {
 }
 
 module.exports.EchoBot = EchoBot;
+
+function levenshteinDistance(s1, s2) {
+    s1 = s1.toLowerCase();
+    s2 = s2.toLowerCase();
+
+    var matrix = [];
+    var i, j;
+
+    // Initialize matrix with the size of the strings
+    for (i = 0; i <= s1.length; i++) {
+        matrix[i] = [i];
+    }
+    for (j = 0; j <= s2.length; j++) {
+        matrix[0][j] = j;
+    }
+
+    // Fill in the rest of the matrix
+    for (i = 1; i <= s1.length; i++) {
+        for (j = 1; j <= s2.length; j++) {
+            if (s1.charAt(i - 1) === s2.charAt(j - 1)) {
+                matrix[i][j] = matrix[i - 1][j - 1];
+            } else {
+                matrix[i][j] = Math.min(
+                    matrix[i - 1][j - 1] + 1, // substitution
+                    matrix[i][j - 1] + 1,     // insertion
+                    matrix[i - 1][j] + 1      // deletion
+                );
+            }
+        }
+    }
+
+    // Return the Levenshtein distance
+    return matrix[s1.length][s2.length];
+}
